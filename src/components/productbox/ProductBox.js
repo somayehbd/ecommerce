572031b82c70905387cd224edfcc6bfe2b949417
somayehbd <somayehbd@gmail.com>
2023-04-04 14:07:23 +0200

    improve: change the style of picture

--- src/components/productbox/ProductBox.js
+++ src/components/productbox/ProductBox.js
@@ -1,9 +1,13 @@
-import noimage from "./assets/productimg.jpg"
+import noimage from "./assets/productimg.png"
 import style from './ProductBox.module.css'
 
 const ProductBox = () => {
     return (<>
         <img className={style.img} src={noimage}></img>
+        <div className={style.productontent}>
+            <p>product title</p>
+            <p>product price</p>
+        </div>
     </>
 
     )
