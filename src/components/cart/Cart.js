fb9990eaa97588ce62c4327a0618071549453013
somayehbd <somayehbd@gmail.com>
2023-04-08 11:27:46 +0200

    feat:added click event to Cart Component

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,8 +2,12 @@ import style from './Cart.module.css'
 import { FaShoppingCart } from 'react-icons/fa';
 
 const Cart = () => {
+    const handleClick = () => {
+        alert('this is shoppingcart')
+    }
+    
     return (
-        <div className={`${style.cart}`}>
+        <div className={`${style.cart}`} onClick={handleClick}>
             <FaShoppingCart />
         </div>
     )
