0b520900c25ab88031b0c2e18bb974ea48f5837d
somayehbd <somayehbd@gmail.com>
2023-04-12 11:19:38 +0200

    refactor:moved provider tag into the app.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import HomeProduct from './components/homeproduct/HomeProduct';
 import Layout from './components/layout/Layout';
 import { Routes, Route } from "react-router-dom"
 import DetailProduct from './components/detailproduct/DetailProduct';
+import { Provider } from 'react-redux';
+import store from './Redux/store'
 
 function App() {
   return (
+
+    <Provider store={store}>
       <Routes>
         <Route path="/" element={<Layout />} >
           <Route path="/" element={<HomeProduct />} />
           <Route path="/detail/:seoName" element={<DetailProduct />} />
         </Route>
       </Routes>
+         </ Provider>
+         
   );
 }
 
