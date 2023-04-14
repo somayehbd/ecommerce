797fae2cb7b7291f93de4a4329b1f77b432ae02c
somayehbd <somayehbd@gmail.com>
2023-04-14 21:50:22 +0200

    improve: updated shoppingCart style

--- src/components/shoppingcart/ShoppingCart.js
+++ src/components/shoppingcart/ShoppingCart.js
@@ -9,31 +9,33 @@ function ShoppingCart() {
         <div className={`container ${style.shoppingCartContainer}`}>
             <div className="row">
                 <div className={`col-12 ${style.shopppingCartHeader}`}>
-                    <p>text left side</p>
+                  <p>shopping Cart</p>
                     <div className={style.closeShoppingcart}> <IoClose /> </div>
                 </div>
             </div>
-            <div className="row">
-                <div className={`col-4 ${style.shopppingCartRemove}`}>
-                    <RiDeleteBin6Fill size={24} />
-                </div>
-                <div className={`col-6 ${style.shopppingCartContent}`} >
-                    <p>product name</p>
-                    <p> product price</p>
-                    <p>display number of selected product</p>
-                </div>
-
-                <div className={`col-2 ${style.shopppingCartImg}`} >
-                    <p>image</p>
+            <div className={`row ${style.contentContainer}`}>
+                <div className="col-12">
+                    <div className={style.flexContainer}>
+                        <div className={style.item1}>
+                            <button><RiDeleteBin6Fill size={24} /></button>
+                        </div>
+                        <div className={style.item2}>
+                            <p>iphone 12</p>
+                            <p>$1200</p>
+                        </div>
+                        <div>
+                            <img src="https://dummyimage.com/45x45/000/fff" />
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="row">
+            <div className={`row ${style.calcContainer}`}>
                 <div className={`col-12 ${style.shopppingCartTotal}`}>
                     <p> TotalPrice:</p>
-                    <p>total price by number</p>
+                    <p>355$</p>
                 </div>
             </div>
-            <div className="row">
+            <div className={`row ${style.btncontainer}`}>
                 <div className="col-12">
                     <a className="btn btn-primary">go user information page to pay</a>
                 </div>
