f59a22c5c8e4be9ace8caab3fb70744c52e3147f
somayehbd <somayehbd@gmail.com>
2023-05-06 21:13:48 +0200

    feat: added home link to top menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
 import style from './Menu.module.css'
-
 
 function Menu() {
     return (
         <ul className={`nav ${style.leftmenu}`}>
             <li className="nav-item">
-                <a className="nav-link active" href="#">Active</a>
+                <Link to={`/`} className="nav-link active">
+                    Home
+                </Link>
             </li>
             <li className="nav-item">
                 <a className="nav-link" href="#">Link</a>
