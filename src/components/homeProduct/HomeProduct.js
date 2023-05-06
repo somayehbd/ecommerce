163b2933284e85d6a34e9cda0dd8d6a0f449c85d
somayehbd <somayehbd@gmail.com>
2023-05-06 21:12:16 +0200

    refactor: added seoName to link

--- src/components/homeProduct/HomeProduct.js
+++ src/components/homeProduct/HomeProduct.js
@@ -7,7 +7,7 @@ import style from './HomeProduct.module.css'
 
 function HomeProduct() {
 
-    const [product, setproduct] = useState([]);
+    const [product, setProduct] = useState([]);
     const dispatch = useDispatch();
     // have different dispatch 
     const handleClick = (id, name, price, image) => {
@@ -20,7 +20,7 @@ function HomeProduct() {
                 return res.json()
             })
             .then(data => {
-                setproduct(data)
+                setProduct(data)
             })
             .catch(error => {
                 console.log("error: ", error)
@@ -28,31 +28,24 @@ function HomeProduct() {
     }, [])
 
     return (
-        <>
-            {
-                product.map(item => {
-                    return (
-                        <div className="col-3" key={item.id}>
-                            <Link to={`/detail/${item.seoName}`}>
-                                <img src={item.image} className={style.image} />
-                            </Link>
-                            <div className={style.productdetail}>
-                                <Link to="/detail">
-                                    <p>{item.name}</p>
-                                </Link>
-                                <p>{"$" + item.price}</p>
-                                <p>{item.description.substring(0, 40)}...</p>
-                                <a href="#" className={`btn btn-primary ${style.button}`} onClick={() => handleClick(item.id, item.name, item.price, item.image)}>ADD TO CART</a>
+        product.map(item => {
+            return (
+                <div className="col-3" key={item.id}>
+                    <Link to={`/detail/${item.seoName}`}>
+                        <img src={item.image} className={style.image} />
+                    </Link>
+                    <div className={style.productdetail}>
+                        <Link to={`/detail/${item.seoName}`}>
+                            <p>{item.name}</p>
+                        </Link>
+                        <p>{"$" + item.price}</p>
+                        <p>{item.description.substring(0, 40)}...</p>
+                        <a href="#" className={`btn btn-primary ${style.button}`} onClick={() => handleClick(item.id, item.name, item.price, item.image)}>ADD TO CART</a>
 
-                            </div>
-                        </div>
-                    )
-
-                })
-
-            }
-
-        </>
+                    </div>
+                </div>
+            )
+        })
     )
 }
 export default HomeProduct
