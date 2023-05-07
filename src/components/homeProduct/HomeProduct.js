d02d315f8a79d3b2823d7478163cfd23bba1c0cb
somayehbd <somayehbd@gmail.com>
2023-05-07 14:36:19 +0200

    refactor: removed unused code

--- src/components/homeProduct/HomeProduct.js
+++ src/components/homeProduct/HomeProduct.js
@@ -24,42 +24,12 @@ function HomeProduct() {
 
     return (
         searchQuery == '' ? products.map(item => {
-            // return (
-            //     <div className="col-3" key={item.id}>
-            //         <Link to={`/detail/${item.seoName}`}>
-            //             <img src={item.image} className={style.image} />
-            //         </Link>
-            //         <div className={style.productdetail}>
-            //             <Link to={`/detail/${item.seoName}`}>
-            //                 <p>{item.name}</p>
-            //             </Link>
-            //             <p>{"$" + item.price}</p>
-            //             <p>{item.description.substring(0, 40)}...</p>
-            //             <a href="#" className={`btn btn-primary ${style.button}`} onClick={() => handleClick(item.id, item.name, item.price, item.image)}>ADD TO CART</a>
-
-            //         </div>
-            //     </div>
-            // )
            return (<HomeProductBox product={item}/>)
         }) : products.filter(item => { // pipline senario
             return item.name.toLowerCase().includes(searchQuery) ;
         }).map(item => {
             return (
                  (<HomeProductBox product={item}/>)
-                // <div className="col-3" key={item.id}>
-                //     <Link to={`/detail/${item.seoName}`}>
-                //         <img src={item.image} className={style.image} />
-                //     </Link>
-                //     <div className={style.productdetail}>
-                //         <Link to={`/detail/${item.seoName}`}>
-                //             <p>{item.name}</p>
-                //         </Link>
-                //         <p>{"$" + item.price}</p>
-                //         <p>{item.description.substring(0, 40)}...</p>
-                //         <a href="#" className={`btn btn-primary ${style.button}`} onClick={() => handleClick(item.id, item.name, item.price, item.image)}>ADD TO CART</a>
-
-                //     </div>
-                // </div>
             )
         })
     )
