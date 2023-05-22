01587431280b3a72687aade11b823edc3acd3a3d
somayehbd <somayehbd@gmail.com>
2023-05-22 23:39:30 +0200

    feat: added ManagementProduct component

--- src/components/management/addProduct/AddProduct.js
+++ src/components/management/addProduct/AddProduct.js
@@ -1,6 +1,13 @@
 const AddProduct=()=>{
     return(
+        <>
+        
         <p>apple</p>
+        <p>apple</p>
+        <p>apple</p>
+        <p>apple</p>
+        <p>apple</p>
+        </>
     )
 }
 export default AddProduct;
