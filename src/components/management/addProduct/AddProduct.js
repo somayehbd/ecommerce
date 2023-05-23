efc939d46ce431f234c4c8b537c3d55bc6e4ae39
somayehbd <somayehbd@gmail.com>
2023-05-23 15:24:05 +0200

    feat: added useForm and displayed inpute value

--- src/components/management/addProduct/AddProduct.js
+++ src/components/management/addProduct/AddProduct.js
@@ -1,20 +1,26 @@
-const AddProduct = () => {
+import { useForm } from 'react-hook-form';
 
+const AddProduct = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
     return (
         <>
             <div className="container">
-                <form id="productForm">
+                <form id="productForm" onSubmit={handleSubmit(onSubmit)}>
                     <div className="form-group">
                         <label for="productName">Product Name:</label>
-                        <input type="text" className="form-control" id="productName" required />
+                        <input type="text" className="form-control" id="productName" {...register("name")} />
                     </div>
                     <div className="form-group">
                         <label for="price">Price:</label>
-                        <input type="number" className="form-control" id="price" required />
+                        <input type="number" className="form-control" id="price" {...register("price")} />
                     </div>
                     <div className="form-group">
                         <label for="shortInfo">Short Info:</label>
-                        <textarea className="form-control" id="shortInfo" required></textarea>
+                        <textarea className="form-control" id="shortInfo" {...register("description")}></textarea>
                     </div>
                     <div className="form-group">
                         <label for="productPicture">Product Picture:</label>
@@ -27,7 +33,7 @@ const AddProduct = () => {
                         </div>
                     </div>
                     <div className="form-group">
-                        <button type="submit" className="btn btn-primary">Save</button>
+                        <button type="submit" className="btn btn-primary">submit</button>
                         <button type="button" className="btn btn-secondary" id="editButton">Edit</button>
                         <button type="button" className="btn btn-danger" id="deleteButton">Delete</button>
                     </div>
