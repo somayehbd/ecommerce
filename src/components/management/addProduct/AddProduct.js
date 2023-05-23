f598f6fc05ec4476dff3375bdc4d49ba64e06b61
somayehbd <somayehbd@gmail.com>
2023-05-23 11:46:10 +0200

    refactor: change class to className

--- src/components/management/addProduct/AddProduct.js
+++ src/components/management/addProduct/AddProduct.js
@@ -1,34 +1,35 @@
 const AddProduct = () => {
+
     return (
         <>
-            <div class="container">
+            <div className="container">
                 <form id="productForm">
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="productName">Product Name:</label>
-                        <input type="text" class="form-control" id="productName" required />
+                        <input type="text" className="form-control" id="productName" required />
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="price">Price:</label>
-                        <input type="number" class="form-control" id="price" required />
+                        <input type="number" className="form-control" id="price" required />
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="shortInfo">Short Info:</label>
-                        <textarea class="form-control" id="shortInfo" required></textarea>
+                        <textarea className="form-control" id="shortInfo" required></textarea>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="productPicture">Product Picture:</label>
-                        <input type="file" class="form-control-file" id="productPicture" />
+                        <input type="file" className="form-control-file" id="productPicture" />
                     </div>
-                    <div class="form-group">
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="checkbox1" />
-                            <label class="form-check-label" for="checkbox1">Checkbox 1</label>
+                    <div className="form-group">
+                        <div className="form-check">
+                            <input type="checkbox" className="form-check-input" id="checkbox1" />
+                            <label className="form-check-label" for="checkbox1">Checkbox 1</label>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary">Save</button>
-                        <button type="button" class="btn btn-secondary" id="editButton">Edit</button>
-                        <button type="button" class="btn btn-danger" id="deleteButton">Delete</button>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary">Save</button>
+                        <button type="button" className="btn btn-secondary" id="editButton">Edit</button>
+                        <button type="button" className="btn btn-danger" id="deleteButton">Delete</button>
                     </div>
                 </form>
             </div>
