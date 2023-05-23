dfc378150a7cfb030ad9b7f114414455847e71fb
somayehbd <somayehbd@gmail.com>
2023-05-23 11:33:32 +0200

    feat: added form for productmanagement

--- src/components/management/addProduct/AddProduct.js
+++ src/components/management/addProduct/AddProduct.js
@@ -1,12 +1,37 @@
-const AddProduct=()=>{
-    return(
+const AddProduct = () => {
+    return (
         <>
-        
-        <p>apple</p>
-        <p>apple</p>
-        <p>apple</p>
-        <p>apple</p>
-        <p>apple</p>
+            <div class="container">
+                <form id="productForm">
+                    <div class="form-group">
+                        <label for="productName">Product Name:</label>
+                        <input type="text" class="form-control" id="productName" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="price">Price:</label>
+                        <input type="number" class="form-control" id="price" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="shortInfo">Short Info:</label>
+                        <textarea class="form-control" id="shortInfo" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="productPicture">Product Picture:</label>
+                        <input type="file" class="form-control-file" id="productPicture" />
+                    </div>
+                    <div class="form-group">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="checkbox1" />
+                            <label class="form-check-label" for="checkbox1">Checkbox 1</label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                        <button type="button" class="btn btn-secondary" id="editButton">Edit</button>
+                        <button type="button" class="btn btn-danger" id="deleteButton">Delete</button>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
