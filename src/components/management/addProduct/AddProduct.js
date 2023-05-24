1cceff28ba94085dce75b324d2fda289e90500e5
somayehbd <somayehbd@gmail.com>
2023-05-24 12:07:31 +0200

    improve: saved data in localStorage

--- src/components/management/addProduct/AddProduct.js
+++ src/components/management/addProduct/AddProduct.js
@@ -4,7 +4,18 @@ const AddProduct = () => {
     const { register, handleSubmit } = useForm();
 
     const onSubmit = (data) => {
-        console.log(data);
+        const storedProducts = localStorage.getItem('products');
+        let products = JSON.parse(storedProducts)
+
+        if (products == null) {
+            products = [data];
+        }
+        products.push(data)
+
+
+        localStorage.setItem('products', JSON.stringify(products))
+
+
     };
     return (
         <>
@@ -12,30 +23,22 @@ const AddProduct = () => {
                 <form id="productForm" onSubmit={handleSubmit(onSubmit)}>
                     <div className="form-group">
                         <label for="productName">Product Name:</label>
-                        <input type="text" className="form-control" id="productName" {...register("name")} />
+                        <input type="text" className="form-control" id="productName" required {...register("name")} />
                     </div>
                     <div className="form-group">
                         <label for="price">Price:</label>
-                        <input type="number" className="form-control" id="price" {...register("price")} />
+                        <input type="number" className="form-control" id="price" required {...register("price")} />
                     </div>
                     <div className="form-group">
                         <label for="shortInfo">Short Info:</label>
-                        <textarea className="form-control" id="shortInfo" {...register("description")}></textarea>
+                        <textarea className="form-control" id="shortInfo"  {...register("description")} required></textarea>
                     </div>
                     <div className="form-group">
                         <label for="productPicture">Product Picture:</label>
-                        <input type="file" className="form-control-file" id="productPicture" />
-                    </div>
-                    <div className="form-group">
-                        <div className="form-check">
-                            <input type="checkbox" className="form-check-input" id="checkbox1" />
-                            <label className="form-check-label" for="checkbox1">Checkbox 1</label>
-                        </div>
+                        <input type="text" className="form-control-file" id="productPicture" />
                     </div>
                     <div className="form-group">
                         <button type="submit" className="btn btn-primary">submit</button>
-                        <button type="button" className="btn btn-secondary" id="editButton">Edit</button>
-                        <button type="button" className="btn btn-danger" id="deleteButton">Delete</button>
                     </div>
                 </form>
             </div>
